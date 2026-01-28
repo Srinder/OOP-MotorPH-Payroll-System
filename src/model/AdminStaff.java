3a5233a07592636f5d8012958b0b68ce4aa571af
Srinder <singhsrinder@gmail.com>
2026-01-28 21:59:20 +0800

    OOP-based MotorPH Payroll System

--- src/model/AdminStaff.java
+++ src/model/AdminStaff.java
@@ -1,7 +1,6 @@
-//Admin Staff Department Class
-/**
- *
- * @author singh
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
 package model;
